1e8cdee051f1ee5f1b6370f02f8976df77fabecc
IsacFSC <128650745+IsacFSC@users.noreply.github.com>
2025-10-09 18:47:24 -0400

    Atualizar o next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  ignoreBuildErrors: true
+  ignoreBuildErrors: true,
+  eslint: { ignoreDuringBuilds: true },
 };
 
 export default nextConfig;
